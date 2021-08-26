e028d6fb02ba9e20ff03f226bd71f6d20e5daef1
tshadz <thais.sombra@hotmail.com>
2021-08-26 18:19:51 -0300

    :package: Funcionalidade inicial de inserçao de alunos

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
 import express from 'express'
 import cors from 'cors'
 import { AddressInfo } from 'net'
+import dotenv from 'dotenv'
+import knex from 'knex'
 
 export const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+dotenv.config()
+
+const connection = knex({
+    client: 'mysql',
+    connection: {
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        multiStatements: true
+    }
+})
+
+
 
 const server = app.listen(process.env.PORT || 3003, () => {
     if(server){

77b05bdf189934f6c29d1a03813c903e50d6edd1
tshadz <thais.sombra@hotmail.com>
2021-08-27 12:50:57 -0300

    :package: Funcionalidade de select de teachers

---
+++ src/endpoints/getTeachers.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express";
+import selectTable from "../data/selectTable";
+
+export const getTeachers = async (req: Request, res: Response) => {
+
+    try {
+
+        const result = await selectTable('teacher')
+
+        if(!result){
+            throw new Error("Docentes não encontrados");
+            
+        }
+
+        const teachers = result.map((teacher: any) => {
+            return {
+                id: teacher.id,
+                nome: teacher.nome,
+                email: teacher.email,
+                data_nasc: teacher.data_nasc,
+                turma_id: teacher.class_id
+            }
+        })
+
+        res.status(200).send(teachers)
+    } catch (error) {
+        res.status(404).send(error.message || error.sqlMessage)
+    }
+}

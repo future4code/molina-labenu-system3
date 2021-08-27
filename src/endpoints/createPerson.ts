601b8871cdabbcaf11199928c9ca43cc7cc34778
tshadz <thais.sombra@hotmail.com>
2021-08-27 16:06:05 -0300

    :arrow_up: Atualizaçao de codigo para deixar endpoint reutilizavel

--- src/endpoints/createPerson.ts
+++ src/endpoints/createPerson.ts
@@ -14,6 +14,10 @@ const createPerson = async(req: Request, res: Response) => {
         if (!nome || !email || !data_nasc || !tipo) {
             throw new Error("Preencha todos os campos obrigatórios")
         }
+
+        if(tipo !== "student" && tipo !== "teacher") {
+            throw new Error("O campo tipo precisa ser preenchido com student ou teacher")
+        }
         
         if (email.indexOf('@') === -1) {
             throw new Error("Digite um email válido")            

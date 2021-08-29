9dece9d7a201a34e746dfca07a26068c30ad2f73
izabellacbrandao <bellacbrandao@gmail.com>
2021-08-29 00:13:17 -0300

    :beetle: corrigindo bug de criar turma

--- src/data/insertClass.ts
+++ src/data/insertClass.ts
@@ -11,7 +11,7 @@ const insertClass = async(
     await connection.raw(`
         INSERT INTO labenu_system_class (id, nome, data_inicio, data_final, modulo )
         VALUES(
-            ${name === "Aluno sem turma" ? "disponivel" : (Date.now() + Math.random().toString())}
+            ${name === "Aluno sem turma" ? "disponivel" : (Date.now() + Math.random().toString())},
             "${shift === "integral" ? name : name+"-na-night"}",
             "${startDate}",
             "${finalDate}",
